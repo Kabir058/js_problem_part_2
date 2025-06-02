3865808925af0f121079b06a4c6c16024627394f
Kabir058 <riduanulkabir01@gmail.com>
2025-06-02 14:31:03 +0600

    checking 3 number and display the bigger one

--- max.js
+++ max.js
@@ -1,4 +1,13 @@
 function getMax(num1, num2)
 {
-    
+    if(num1>num2)
+    {
+        return num1;
+    }
+    else{
+        return num2;
+    }
 }
+
+let number = getMax(34,564)
+console.log(number)
---
+++ max3.js
@@ -0,0 +1,15 @@
+let jenny = 454;
+let simi = 43;
+let marry = 543;
+if(jenny>simi && jenny>marry)
+{
+    console.log( "Jenny is the superior")
+}
+ else if(simi>jenny && simi>marry)
+ {
+    console.log("Simi is the lady boss")
+ }
+
+ else{
+    console.log(" I love you marry")
+ }
