c44866167d2e107ea69a7a4af2f60f6e9d60875f
Kabir058 <riduanulkabir01@gmail.com>
2025-06-04 22:20:20 +0600

    Fix: Access product price correctly in loop to calculate total sum

---
+++ obTotal.js
@@ -0,0 +1,17 @@
+let pro =[
+    { name : 'panjabi', price : 4200},
+    { name : 'Shirt', price : 1600},
+    { name : 'Perfume', price : 14000},
+    { name : 'Shoe', price : 4000}
+]
+
+function shoptotal(products){
+    let sum = 0;
+    for(i=0; i<products.length ; i++ )
+    {
+         sum += products[i].price;
+    } return sum;
+}
+
+let tot = shoptotal(pro)
+console.log(tot)
