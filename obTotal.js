449382888e99b506f7b5c2bff40fa04a459adc13
Kabir058 <riduanulkabir01@gmail.com>
2025-06-04 22:21:57 +0600

    Fix: Access product price correctly using for of loop through function

--- obTotal.js
+++ obTotal.js
@@ -1,17 +1,37 @@
-let pro =[
-    { name : 'panjabi', price : 4200},
-    { name : 'Shirt', price : 1600},
-    { name : 'Perfume', price : 14000},
-    { name : 'Shoe', price : 4000}
-]
+// let pro =[
+//     { name : 'panjabi', price : 4200},
+//     { name : 'Shirt', price : 1600},
+//     { name : 'Perfume', price : 14000},
+//     { name : 'Shoe', price : 4000}
+// ]
 
-function shoptotal(products){
+// function shoptotal(products){
+//     let sum = 0;
+//     for(i=0; i<products.length ; i++ )
+//     {
+//          sum += products[i].price;
+//     } return sum;
+// }
+
+// let tot = shoptotal(pro)
+// console.log(tot)
+
+// using for of loop
+
+let pro = [
+    { name: 'panjabi', price: 4200 },
+    { name: 'Shirt', price: 1600 },
+    { name: 'Perfume', price: 14000 },
+    { name: 'Shoe', price: 4000 }
+];
+
+function shoptotal(products) {
     let sum = 0;
-    for(i=0; i<products.length ; i++ )
-    {
-         sum += products[i].price;
-    } return sum;
+    for (let item of products) {
+        sum += item.price;
+    }
+    return sum;
 }
 
-let tot = shoptotal(pro)
-console.log(tot)
+let tot = shoptotal(pro);
+console.log(tot);
