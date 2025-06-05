f51a780275afa1fa063f900e10c51e02a7a9ea59
Kabir058 <riduanulkabir01@gmail.com>
2025-06-05 23:51:00 +0600

    feat: Use array of objects to store device info and calculate total

---
+++ task3.js
@@ -0,0 +1,15 @@
+// Task-3:
+// Your task is to calculate the total budget required to buy electronics:
+
+//     laptop = 35000 tk
+//     tablet = 15000 tk
+//     mobile = 20000 tk
+
+let devices = {
+    laptop: 35000,
+    tablet: 15000,
+    mobile: 20000
+};
+
+let total = devices.laptop + devices.tablet + devices.mobile;
+console.log("Total cost: " + total + " tk"); 
